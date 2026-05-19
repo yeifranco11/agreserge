dafc12710d0552df7b91f764f15617e72d3dd77a
YEIDER FRANCO FRANCO <140337289+yeifrancoo77@users.noreply.github.com>
2026-05-19 11:10:53 -0500

    Excluir carpeta next

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"cFbDRWUuOLXH1hGqHKz7aLHmGfeaTHFPET5k7C4YBKw=\"\n}"
--- .next/dev/static/development/_clientMiddlewareManifest.js
+++
@@ -1,6 +0,0 @@
-self.__MIDDLEWARE_MATCHERS = [
-  {
-    "regexp": "^(?:\\/(_next\\/data\\/[^/]{1,}))?\\/intranet(?:\\/((?:[^\\/#\\?]+?)(?:\\/(?:[^\\/#\\?]+?))*))?(\\.json|\\.rsc|\\.segments\\/.+\\.segment\\.rsc)?[\\/#\\?]?$",
-    "originalSource": "/intranet/:path*"
-  }
-];self.__MIDDLEWARE_MATCHERS_CB && self.__MIDDLEWARE_MATCHERS_CB()
